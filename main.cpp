599f8c70b88a971768f28f119738b60a306ea102
Nick Korotysh <nick.korotysh@gmail.com>
2023-05-02 00:51:14 +0200

    app initialization fixes

--- main.cpp
+++ main.cpp
@@ -4,13 +4,19 @@
 
 int main(int argc, char *argv[])
 {
+#if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+#endif
+    QApplication a(argc, argv);
+
     QApplication::setApplicationName(QStringLiteral("Font Preview"));
     QApplication::setApplicationDisplayName(QApplication::applicationName());
     QApplication::setOrganizationName(QStringLiteral("Nick Korotysh"));
-    QApplication::setOrganizationDomain(QStringLiteral("com.github.kolcha.font-preview"));
+    QApplication::setOrganizationDomain(QLatin1String("kolcha.github.com"));
+    QApplication::setDesktopFileName(QLatin1String("com.github.kolcha.font-preview"));
     QApplication::setApplicationVersion(QStringLiteral("0.1.0"));
 
-    QApplication a(argc, argv);
     const auto args = QApplication::arguments().mid(1);
     MainWindow w(args.isEmpty() ? QString() : args.first());
     w.show();
